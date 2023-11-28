575f15d0ea09673725fc29ed21d79fa040da7240
David Fonseca <davidfonsecagaona@gmail.com>
2023-11-28 15:08:25 -0600

    Adding Team Assignment - Module 9

--- Module9/Lab Activity/Problem1Infinite.py
+++ Module9/Lab Activity/Problem1Infinite.py
@@ -3,6 +3,10 @@
 # Problem 1 - infinite loop that prints “Infinite”. An infinite loop never ends. The
 # condition is always true.
 
-while True:
-    print("Infinite")
+n = 10
+answer = 1
+while n < 15:
+                    answer = answer + n
+                    n = n + 1
+print(answer)
 
